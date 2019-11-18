cc26a463e0926a2140b9f06e635e6987ee0f885b
Alessandro <trofui@gmail.com>
2019-11-18 22:34:13 +0100

    aggiunta commenti

--- fachiri/fachiri.c
+++ fachiri/fachiri.c
@@ -12,16 +12,23 @@ pthread_t tid[NUM_FACHIRI];
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void* prendi_ucciditi(void *arg){
+	/* Dichiarazione variabili */
 	int i;
 	int num_fachiro;
+	/* cast *arg --> *int --> *int */  
 	num_fachiro = *((int*)arg);
+	/* loop infinito */
 	while(1){
+		/* per ogni spada */
 		for(i = 0; i < NUM_SPADE; i++){
 			pthread_mutex_lock(& mutex);
+			/* prendo la spada */
 			spada[i] = 1;
 			printf("**Sono fachiro %d mi sto per trafiggere con la %d spada**\n",num_fachiro+1,i+1);
+			/* la prendo in mano */
 			sleep(1);
 			printf("AAAAAAAAAAAAAAAAAAAAAAAAAUUUUUUUUUUUUUUUU\n");
+			/* la poso */
 			spada[i] = 0;
 			pthread_mutex_unlock(& mutex);
 			sleep(1);
@@ -30,12 +37,17 @@ void* prendi_ucciditi(void *arg){
 }
 
 int main(void){
-	int i;
+	int i,rc;
 	int *p;
 	for(i = 0; i < NUM_FACHIRI; i++){
 		p = (int*) malloc(sizeof(int));
 		*p = i;
-		pthread_create(&tid[i], NULL, prendi_ucciditi, (void*) p);
+		rc = pthread_create(&tid[i], NULL, prendi_ucciditi, (void*) p);
+		if(rc){
+			char msg[256];
+			strerror_r(rc, msg, 256);
+			puts(msg);
+		}
 	}
 	pthread_exit(NULL);
 }
