0e5cef95dc9c489116ed4967003c4acaacf30196
Alessandro <trofui@gmail.com>
2019-11-18 23:20:10 +0100

    modification

---
+++ fachiri/3bis-fachiri/fachiri3.c
@@ -0,0 +1,74 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#define NUM_FACHIRI 2
+#define NUM_SPADE 10
+
+int spada[NUM_SPADE];
+pthread_t tid[NUM_FACHIRI];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void* prendi_ucciditi(void *arg){
+	/* Dichiarazione variabili */
+	int i;
+	int num_fachiro;
+	/* cast *arg --> *int --> *int */  
+	num_fachiro = *((int*)arg);
+	/* loop infinito */
+	while(1){
+		if(*(int*) arg == 0){
+			/* per ogni spada */
+			for(i = 0; i < NUM_SPADE; i++){
+				pthread_mutex_lock(& mutex);
+				/* prendo la spada */
+				spada[i] = 1;
+				printf("**Sono fachiro %d mi sto per trafiggere con la %d spada**\n",num_fachiro+1,i+1);
+				/* la prendo in mano */
+				sleep(1);
+				printf("AAAAAAAAAAAAAAAAAAAAAAAAAUUUUUUUUUUUUUUUU\n");
+				/* la poso */
+				spada[i] = 0;
+				pthread_mutex_unlock(& mutex);
+				sleep(1);
+			}
+		}
+		else {
+			for(i = NUM_SPADE-1; i >= 0; i--){	
+				pthread_mutex_lock(& mutex);
+				/* prendo la spada */
+				spada[i] = 1;
+				printf("**Sono fachiro %d mi sto per trafiggere con la %d spada**\n",num_fachiro+1,i+1);
+				/* la prendo in mano */
+				sleep(1);
+				printf("AAAAAAAAAAAAAAAAAAAAAAAAAUUUUUUUUUUUUUUUU\n");
+				/* la poso */
+				spada[i] = 0;
+				pthread_mutex_unlock(& mutex);
+				sleep(1);
+			}
+		}
+		
+	}
+}
+
+int main(void){
+	int rc;
+	int *p;
+	/*MODIFICA*/
+	int i;
+	for(i = 0; i < NUM_FACHIRI; i++){
+		p = (int*) malloc(sizeof(int));
+		*p = i;
+		rc = pthread_create(&tid[i], NULL, prendi_ucciditi, (void*) p);
+		if(rc){
+			char msg[256];
+			strerror_r(rc, msg, 256);
+			puts(msg);
+		}
+	}
+	pthread_exit(NULL);
+}
+	
