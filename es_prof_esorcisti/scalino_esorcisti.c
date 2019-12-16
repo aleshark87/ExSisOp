075b0b1c454d8cc60cbad6de8c59a0225f19350d
aleshark87 <aleshark87@github.com>
2019-12-16 15:04:31 +0100

    ex_prof_esorcisti add

---
+++ es_prof_esorcisti/scalino_esorcisti.c
@@ -0,0 +1,75 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond2 = PTHREAD_COND_INITIALIZER;
+pthread_cond_t fineIntervento = PTHREAD_COND_INITIALIZER;
+int stoBene = 1;
+int aiutantiArrivati = 0;
+
+void* professore(void* arg){
+	while(1){
+		printf("INCIAMPO NELLA SCALA ");
+		printf("PORCO ***, PORCA ******* \n");
+		pthread_mutex_lock(&mutex);
+		printf("chiamo i soccorritori \n");
+		stoBene = 0; 
+		pthread_cond_broadcast(&cond);
+		
+		while(aiutantiArrivati < 2) { 
+			pthread_cond_wait(&cond2, &mutex); 
+		}
+		printf("mi curano \n");
+		sleep(2);
+		aiutantiArrivati = 0; pthread_cond_broadcast(&fineIntervento);
+		pthread_mutex_unlock(&mutex);
+		printf("FACCIO LEZIONE \n");
+		sleep(4);
+	}
+}
+
+void* medico(void* arg){
+	while(1){
+		pthread_mutex_lock(&mutex);
+		while(stoBene == 1){
+			printf("ASPETTO \n");
+			pthread_cond_wait(&cond, &mutex);
+		}
+		aiutantiArrivati++;
+		if(aiutantiArrivati == 2){
+			pthread_cond_signal(&cond2);
+		}
+		pthread_cond_wait(&fineIntervento, &mutex);
+		pthread_mutex_unlock(&mutex);
+	}
+}
+
+void* esorcista(void* arg){
+	while(1){
+		pthread_mutex_lock(&mutex);
+		while(stoBene == 1){
+			printf("ASPETTO \n");
+			pthread_cond_wait(&cond, &mutex);
+		}
+		aiutantiArrivati++;
+		if(aiutantiArrivati == 2){
+			pthread_cond_signal(&cond2);
+		}
+		pthread_cond_wait(&fineIntervento, &mutex);
+		pthread_mutex_unlock(&mutex);
+	}
+}
+
+
+int main(void){
+	pthread_t pid; 
+	pthread_create(&pid, NULL, professore, NULL);
+	pthread_create(&pid, NULL, medico, NULL);
+	pthread_create(&pid, NULL, esorcista, NULL);
+	pthread_exit(NULL);
+}
+		
